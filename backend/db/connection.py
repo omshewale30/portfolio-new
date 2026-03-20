6ed12e28318557ef6e2f5fb0cd5aa66261e3eeb5
omshewale <omshewale030@gmail.com>
2026-03-20 01:32:59 -0400

    db-3

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,8 +1,9 @@
-from sqlalchemy.ext.asyncio import create_engine
+from tkinter import N
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.pool import NullPool
 
 from config import settings
 
 DATABASE_URL = settings.database_url
 
-engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
+engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
