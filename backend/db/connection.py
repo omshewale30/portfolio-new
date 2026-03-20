caf52e6712dc8dfa4138e8e902db4ecc7d31b8de
omshewale <omshewale030@gmail.com>
2026-03-20 01:08:11 -0400

    db fix 1

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,13 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.pool import NullPool
 
 from config import settings
 
-DATABASE_URL = (
-    settings.database_url
-    .replace("postgresql://", "postgresql+asyncpg://")
-    .replace("sslmode=require", "ssl=true")
+engine = create_async_engine(
+    settings.database_url,
+    poolclass=NullPool,
 )
-
-engine = create_async_engine(DATABASE_URL)
-
-print("Connection successful!")
